88b24d12d99dc550d7572aead52750e3eabb33e4
Emanuel Azage <eazage@usc.edu>
2019-03-20 16:19:01 -0700

    added doc page for websocket cliet

--- itp-tunes/app/Http/Controllers/DocController.php
+++ itp-tunes/app/Http/Controllers/DocController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DOMDocument;
 
 class DocController extends Controller
 {
--- itp-tunes/resources/views/doc.blade.php
+++ itp-tunes/resources/views/doc.blade.php
@@ -4,6 +4,35 @@
 
 @section('main')
 
-<div id="doc" contenteditable="true"></div>
+<div style="background-color:silver" id="doc" contentEditable="true"></div>
+
+<script>
+  let connection = new WebSocket('wss://eazage-websocket-server.herokuapp.com') || new WebSocket('ws://localhost:8080');
+
+  connection.onopen = () => {
+    console.log('connected from the frontend');
+  };
+
+  connection.onerror = () => {
+    console.log('failed to connect from the frontend');
+  };
+
+  connection.onmessage = (event) => {
+    console.log(`recieved message: ${event.data}`);
+    let div = document.getElementById('doc');
+    div.innerHTML = event.data;
+    // div.selectionStart = div.selectionEnd = div.innerHTML.length;
+    console.log(div.childNodes);
+  };
+
+  document.getElementById('doc').addEventListener('input', (event) => {
+    event.preventDefault();
+    let message = document.getElementById('doc').innerHTML;
+    message = message.replace("<div><br></div>", "<br>");
+    console.log("client sending:", message);
+    connection.send(message);
+  });
+
+</script>
 
 @endsection
